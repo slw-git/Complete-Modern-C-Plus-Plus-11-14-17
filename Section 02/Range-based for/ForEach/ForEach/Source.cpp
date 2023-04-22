af36972a6c5605282104432c8d4eedfcc54eb8f5
Heikki Metsäpuro <software@metsapuro.com>
2023-04-22 17:32:58 +0300

    Video changes.

--- Section 02/Range-based for/ForEach/ForEach/Source.cpp
+++ Section 02/Range-based for/ForEach/ForEach/Source.cpp
@@ -1,16 +1,46 @@
 #include <iostream>
+
 int main() {
+
 	using namespace std;
+
 	int arr[] = { 1,2,3,4,5 };
+
 	for (int i = 0; i < 5; i++) {
-		cout << arr[i] << " ";
+		std::cout << arr[i] << " ";
 	}
+	std::cout << endl;
+
 	for (const auto & x : arr) {
 		
-		cout << x << " ";
+		std::cout << x << " ";
 	}
-	for (auto x : { 1,2,3,4 }) {
-		cout << x;
+	std::cout << endl;
+
+	for (auto x : { 1,2,3 }) {
+		std::cout << x;
 	}
+	std::cout << endl;
+
+	// Pointerfun with rangers
+	std::cout << "Pointerfun with rangers" << endl;
+	int* beg = std::begin(arr);
+	int* end = std::end(arr);
+	while (beg != end) {
+		std::cout << *beg << " ";
+		beg++;
+	}
+	std::cout << endl;
+
+	std::cout << "Pointerfun with rangers, round 2" << endl;
+	auto &&range = arr;
+	auto beg2 = std::begin(arr);
+	auto end2 = std::end(arr);
+	for (; beg2 != end2; beg2++) {
+		auto v = *beg2;
+		std::cout << v << " ";
+
+	}
+
 	return 0;
 }
