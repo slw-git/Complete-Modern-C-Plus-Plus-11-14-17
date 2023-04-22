61f813e9b87bb2b0a28a34e30c193ce27e18ccb0
Heikki Metsäpuro <software@metsapuro.com>
2023-04-22 17:08:39 +0300

    Video edits.

--- Section 02/auto/AutoKeyword/AutoKeyword/Source.cpp
+++ Section 02/auto/AutoKeyword/AutoKeyword/Source.cpp
@@ -1,22 +1,34 @@
 #include <iostream>
+
 int Sum(int x, int y) {
 	return x + y;
 }
+
+
 int main() {
+
+	using namespace std;
+
 	//One of my favorite features of C++11 ;)
 	auto i = 10;
-	auto j = 5;
-	auto sum = i + 4.3f;
+	auto j = 5.5f;
 
+	cout << "j: " << typeid(j).name() << endl;
+
+	auto sum = i + 4.3f;
 	auto result = Sum(i, j);
+
 	static auto y = 2;
 	const int x = 10;
+	
 	//Works with qualifiers
 	const auto var = x;
+	
 	//Deduced to reference
 	auto &var1 = x;
 
 	//Deduced to a pointer. * is not necessary
 	auto *ptr = &x;
+
 	return 0;
 }
